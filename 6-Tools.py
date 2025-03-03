00554fb49d85d1ced53890da936312c5a62254a8
Ujjwal Vaish <ujjwalvaish5@gmail.com>
2025-03-03 08:27:08 -0500

    Improved the comments and changed the LLM to mistral:7b for Tut-6

--- 6-Tools.py
+++ 6-Tools.py
@@ -23,17 +23,17 @@ def sales_after_discount(orig_price: float, discount_percentage: float ) -> floa
     price after discount
 
     Args:
-        discount_percentage(float) : Percentage discount on the item e.g. A value of 20% discount on 100 should return 80
+        discount_percentage(float) : Percentage discount on the item e.g. A value of 20% discount on 100 = a discount of 20
         orig_price(float) : Price before applying discounts
 
     Returns:
         Final Price
     """
     if discount_percentage > 100:
-        raise ValueError("Discount percentage must be between 0 and 1")
+        raise ValueError("Discount percentage must be between 0 and 100")
     return orig_price * (1 - discount_percentage/100)
 
-
+# Check tool parameters
 # print(sales_after_discount.name) # Name of tool aka function
 # print(sales_after_discount.description)
 # print(sales_after_discount.args)
@@ -46,19 +46,20 @@ def sales_after_discount(orig_price: float, discount_percentage: float ) -> floa
 Tool Calling - combining these tools with LLMs
 1. Model binding
 2. Model when prompted makes a decision if it wants to use any of the bound tools
+3. If a tool needs to be used, we can directly pass the value of "tool_calls[0]['args'] to the tool 
 '''
-print("TOOL IS NOT NEEDED")
+
 llm_with_tools = llm.bind_tools([sales_after_discount])
 
-# TODO - Buggy - sometimes the model uses the tool even if the prompt is "Hi there!"
+print("TOOL IS NOT NEEDED")
 response = llm_with_tools.invoke("How can you help me?")
-print(f"Content - {response.content}")
+print(f"Content - {response.content}") # should have content
 print(f"tool_calls = {response.tool_calls}") # should be empty
 
 print("TOOL IS NEEDED")
 response = llm_with_tools.invoke("What would the final price for a member, if the original item  \
-                           is priced at 200 and there is a 15 percent discount?")
+                           is priced at 200 and there is a 15%  discount?")
 print(f"Content - {response.content}") # should be empty
 print(f"tool_calls = {response.tool_calls}") # dictionary with everything needed to call a tool
 args = response.tool_calls[0]['args']
-print(sales_after_discount.invoke(args))
+print(sales_after_discount.invoke(args)) # manuall calling tools
